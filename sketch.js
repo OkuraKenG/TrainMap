b6631de7cbba32fdf25ff542ecb9bb732492901b
Kenji <70079754+OkayKenji@users.noreply.github.com>
2023-08-09 21:40:18 -0400

    Revert "Refactoring folders"
    This reverts commit b1eb37fdc14b7b185e56f3876c8835a7589fcea0.

--- sketch.js
+++ sketch.js
@@ -10,12 +10,12 @@ let linesmap;
 function preload() {
   abc = loadJSON('./setup.json');
   railroad = 'MNR';
-  calendar_dates = loadTable(`./data/LIRR/calendar_dates.txt`, "csv", "header");
-  routes = loadTable(`./data/LIRR/routes.txt`, "csv", "header");
-  table = loadTable(`./data/LIRR/shapes.txt`, "csv", "header");
-  stop_times = loadTable(`./data/LIRR/stop_times.txt`, "csv", "header");
-  stationList = loadTable(`./data/LIRR/stops.txt`, "csv", "header");
-  trips = loadTable(`./data/LIRR/trips.txt`, "csv", "header");
+  calendar_dates = loadTable(`./data/${railroad}/calendar_dates.txt`, "csv", "header");
+  routes = loadTable(`./data/${railroad}/routes.txt`, "csv", "header");
+  table = loadTable(`./data/${railroad}/shapes.txt`, "csv", "header");
+  stop_times = loadTable(`./data/${railroad}/stop_times.txt`, "csv", "header");
+  stationList = loadTable(`./data/${railroad}/stops.txt`, "csv", "header");
+  trips = loadTable(`./data/${railroad}/trips.txt`, "csv", "header");
   gridmap = loadJSON(`./data/stops.json`);
   linesmap = loadJSON(`./data/lines.json`);
 }
